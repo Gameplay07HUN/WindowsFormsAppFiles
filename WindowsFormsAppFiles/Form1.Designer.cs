a15486489b8e03f35f46aaa9f1cbeceab35d3299
Gameplay07HUN <gameplay719@gmail.com>
2022-12-12 09:09:16 +0100

    Add project files.

---
+++ WindowsFormsAppFiles/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsAppFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxSzoveg = new System.Windows.Forms.RichTextBox();
+            this.textBoxNev = new System.Windows.Forms.TextBox();
+            this.labelNev = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // richTextBoxSzoveg
+            // 
+            this.richTextBoxSzoveg.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBoxSzoveg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.richTextBoxSzoveg.Location = new System.Drawing.Point(0, 111);
+            this.richTextBoxSzoveg.Name = "richTextBoxSzoveg";
+            this.richTextBoxSzoveg.Size = new System.Drawing.Size(800, 339);
+            this.richTextBoxSzoveg.TabIndex = 0;
+            this.richTextBoxSzoveg.Text = "";
+            // 
+            // textBoxNev
+            // 
+            this.textBoxNev.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxNev.Location = new System.Drawing.Point(149, 58);
+            this.textBoxNev.Name = "textBoxNev";
+            this.textBoxNev.Size = new System.Drawing.Size(269, 26);
+            this.textBoxNev.TabIndex = 1;
+            // 
+            // labelNev
+            // 
+            this.labelNev.AutoSize = true;
+            this.labelNev.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelNev.Location = new System.Drawing.Point(103, 61);
+            this.labelNev.Name = "labelNev";
+            this.labelNev.Size = new System.Drawing.Size(40, 20);
+            this.labelNev.TabIndex = 2;
+            this.labelNev.Text = "Név:";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonSave.Location = new System.Drawing.Point(424, 56);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(73, 30);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Mentés";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonOpen.Location = new System.Drawing.Point(529, 58);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(81, 30);
+            this.buttonOpen.TabIndex = 4;
+            this.buttonOpen.Text = "Megyitás";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.labelNev);
+            this.Controls.Add(this.textBoxNev);
+            this.Controls.Add(this.richTextBoxSzoveg);
+            this.Name = "Form1";
+            this.Text = "WindowsFormsAppFiles";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxSzoveg;
+        private System.Windows.Forms.TextBox textBoxNev;
+        private System.Windows.Forms.Label labelNev;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ WindowsFormsAppFiles/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsAppFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxNev.Text))
+            {
+                MessageBox.Show("Nem adott meg nevet!");
+                return;
+            }
+            if (string.IsNullOrEmpty(richTextBoxSzoveg.Text))
+            {
+                MessageBox.Show("Nem adott meg szöveget!");
+                return;
+            }
+
+            saveFileDialog1.Filter = "Egyszerű szöveg fájl (*.txt)|*.txt|Veszővel tagolt szövegfájl (*.csv) |*.csv|Minden fájl|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string szoveg = string.Join(";",textBoxNev.Text,richTextBoxSzoveg.Text);
+                string kiveFile=saveFileDialog1.FileName;
+                File.WriteAllText(kiveFile, szoveg);
+                //MessageBox.Show("A kiválaszott fájl:" + kiveFile);
+                textBoxNev.Text = "";
+                richTextBoxSzoveg.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Nincs kiválasztva!");
+            }
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string kivFile = openFileDialog1.FileName;
+                string beolvasottSzoveg = File.ReadAllText(kivFile);
+                string[] adatok = beolvasottSzoveg.Split(';');
+                textBoxNev.Text = adatok[0];
+                richTextBoxSzoveg.Text = adatok[1];
+            }
+        }
+    }
+}
